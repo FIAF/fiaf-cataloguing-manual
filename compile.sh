c9f6183809a9fa53a920761f49a63ea9059a785a
paulduchesne <paulduchesne@tuta.io>
2024-05-12 20:13:35 +1000

    mermaid tests

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# pull font if required.
+
 if [ ! -d "brownstd" ]; then
     echo "pulling font..."
     curl -O https://font.download/dl/font/brownstd.zip
@@ -8,8 +10,30 @@ if [ ! -d "brownstd" ]; then
     cp -r brownstd ~/.fonts/brownstd
 fi
 
+# render mermaid.
+
+# mmdc -i mermaid/e.5.1.1/en.mmd -o mermaid/e.5.1.1/en.svg  --width 800 -c ~/.config/mermaid/config.json
+
+
+# pandoc -i mermaid/e.5.1.1/test.html --pdf-engine=xelatex -o manual.pdf --template=config.tex
+
+
+# render pdf.
+
 echo "building pdf..."
 MARKDOWN=$(find markdown -type f -name '*.md' | sort)
 pandoc $MARKDOWN --pdf-engine=xelatex -o manual.pdf --template=config.tex
 
 echo "finished."
+
+
+# {
+#   "flowchart": {
+#     "useMaxWidth": false,
+#     "htmlLabels": false
+#   }
+# }
+
+# mmdc -i mermaid/e.5.1.1/en.mmd -o mermaid/e.5.1.1/en.svg -c ~/.config/mermaid/config.json
+
+# mmdc -c ~/.config/mermaid/config.json
