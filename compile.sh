9b4ccab9da5ffcd422ac2e0788e474d7e86ba5d6
paulduchesne <paulduchesne@tuta.io>
2024-05-13 21:57:46 +1000

    switched to puppeteer

--- compile.sh
+++ compile.sh
@@ -10,42 +10,15 @@ if [ ! -d "brownstd" ]; then
     cp -r brownstd ~/.fonts/brownstd
 fi
 
-# render mermaid.
+# render diagrams.
 
-# mmdc -i mermaid/e.5.1.1/en.mmd -o mermaid/e.5.1.1/en.svg  --width 800 -c ~/.config/mermaid/config.json
-
-
-# pandoc -i mermaid/e.5.1.1/test.html --pdf-engine=xelatex -o manual.pdf --template=config.tex
 npm install jsdom
 npm install d3@6
-npm install puppeteer
-npm install fontfaceobserver
-
-# npm install d3-annotation
-npm i d3-svg-annotation -S
-
 node diagrams/E.5.1.1/en.js
-
-
-# maybe do a svg conversion here?
 
 # render pdf.
 
-# echo "building pdf..."
-# MARKDOWN=$(find markdown -type f -name '*.md' | sort)
-# pandoc $MARKDOWN --pdf-engine=xelatex -o manual.pdf --template=config.tex
-# # pandoc $MARKDOWN --pdf-engine=wkhtmltopdf -o manual.pdf --pdf-engine-opt=--enable-local-file-access
-
-# echo "finished."
-
-
-# {
-#   "flowchart": {
-#     "useMaxWidth": false,
-#     "htmlLabels": false
-#   }
-# }
-
-# mmdc -i mermaid/e.5.1.1/en.mmd -o mermaid/e.5.1.1/en.svg -c ~/.config/mermaid/config.json
-
-# mmdc -c ~/.config/mermaid/config.json
+echo "building pdf..."
+MARKDOWN=$(find markdown -type f -name '*.md' | sort)
+pandoc $MARKDOWN --pdf-engine=xelatex -o manual.pdf --template=config.tex
+echo "finished."
--- diagrams/E.5.1.1/en.js
+++ diagrams/E.5.1.1/en.js
@@ -1,121 +1,26 @@
-
-
-// mission here is to create svg to disk, then worry about insertion
-
-var jsdom = require('jsdom');
-const { JSDOM } = jsdom;
-const d3 = require('d3');
+const puppeteer = require('puppeteer');
 const fs = require('fs');
+const path = require('path');
 
+(async () => {
+    const browser = await puppeteer.launch();
 
-const dom = new JSDOM(`<!DOCTYPE html>
+    const page = await browser.newPage();
 
+    const indexPath = path.resolve(__dirname, 'en.html');
+    await page.goto('file://' + indexPath);
 
-<head>
-    <title>FIAFcore</title>
-  
-    <link href="https://fonts.googleapis.com/css?family=Pacifico:100,200,300,400,500,600,700,800,900"
-        rel="stylesheet" />
+    await page.waitForSelector('svg');
 
-            <style>
-        
-              		@font-face{
-          			font-family: "Brown";
-          			src: url("./BrownStd-Regular.ttf") format("truetype");
-          		}	
-             </style>
+    const svgContent = await page.evaluate(() => {
+        const svgElement = document.querySelector('svg');
+        return svgElement.outerHTML;
+    });
 
-</head>
+    const outPath = path.resolve(__dirname, 'en.svg');
+    fs.writeFileSync(outPath, svgContent);
 
+    await browser.close();
 
-
-<body></body>`);
-
-
-
-let body = d3.select(dom.window.document.querySelector("body"))
-let svg = body.append('svg').attr('width', 800).attr('height', 800).attr('xmlns', 'http://www.w3.org/2000/svg');
-
-
-svg.append("rect")
-    .attr("x", 10)
-    .attr("y", 10)
-    .attr("width", 800)
-    .attr("height", 500)
-    .style("fill", "white");
-
-
-
-const nodes = [
-
-
-    {'top': 'hello', 'cla':'sab', 'lines': [
-        {"text":"Sabrina","x":100, "y":50},
-        {"text":"(1954) (Work)","x":100, "y":50}
-    ]},
-
-    {'top': 'hello','cla':'aud', 'lines': [
-    
-        {"text":"The Audrey Hepburn Collection","x":300, "y":50},
-        {"text":"(2008) (Work)","x":300, "y":50}
-    ]},
-
-
-
-    {'top': 'hello', 'cla':'fun', 'lines': [
-        {"text":"Funny Face","x":500, "y":50},
-        {"text":"(1956) (Work)","x":500, "y":50}
-    ]},
-] 
-
-
-
-nodes.forEach(
-    d => { 
-        
-        
-        
-        
-        console.log(d);
-    
-    
-    
-    
-    
-
-
-
-          svg.selectAll('g')
-          .data(d['lines'])
-          .join('text')
-          .attr("class", d.cla) // class should be at box level
-          .attr('x', d => d.x)
-          .attr('y', (d,i) => (d.y)+(i*20))
-          .attr('font-family', 'brownstd')
-          .attr('font-size', '12px')
-          .attr('font-weight', '100')
-          .attr('text-anchor', 'middle')
-          .attr('alignment-baseline', 'middle')
-          .attr('stroke', 'black')
-          .attr('fill', 'black')
-          .text(d => d.text)
-    })
-
-
-
-
-    // console.log(d3.select('#sab').node())
-
-//  get a bbox for each class
-
-// nodes.forEach(d => 
-//     { d.box = d3.select("#"+d.class).node().getBBox()}
-// )
-  
-
-// console.log(nodes)
-
-
-fs.writeFileSync('diagrams/E.5.1.1/en.svg', body.html());
-
-
+    console.log('SVG graph saved successfully!');
+})();
