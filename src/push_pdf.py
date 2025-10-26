eaef057f33d56a83daa8f08a8fd5b9d473ec2d83
Paul Duchesne <paulduchesne@tuta.io>
2025-10-26 21:23:18 +1100

    Update push_pdf.py

--- src/push_pdf.py
+++ src/push_pdf.py
@@ -1,2 +1,15 @@
 from b2sdk.v2 import InMemoryAccountInfo
 from b2sdk.v2 import B2Api
+import os
+
+APP_KEY = os.environ["BACKBLAZE_APP_KEY"]
+APP_KEY_ID = os.environ["BACKBLAZE_APP_KEY_ID"]
+
+info = InMemoryAccountInfo()
+b2_api = B2Api(info)
+b2_api.authorize_account("production", APP_KEY_ID, APP_KEY)
+
+buck = b2_api.get_bucket_by_name("cataloguing-manual")
+print(buck)
+
+
