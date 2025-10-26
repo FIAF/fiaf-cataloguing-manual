1801c8a418e5899268ca296ab4b6082351d4283b
Paul Duchesne <paulduchesne@tuta.io>
2025-10-26 21:51:28 +1100

    Update push_pdf.py

--- src/push_pdf.py
+++ src/push_pdf.py
@@ -16,8 +16,8 @@ b2_api = B2Api(info)
 b2_api.authorize_account("production", APP_KEY_ID, APP_KEY)
 
 buck = b2_api.get_bucket_by_name("cataloguing-manual")
-buck.upload_local_file(pathlib.Path.cwd() / 'src' / 'render' / 'manual_en.pdf', 'test.pdf')
-link = buck.get_download_url('test.pdf')
+buck.upload_local_file(pathlib.Path.cwd() / 'src' / 'render' / 'manual_en.pdf', PDF_NAME)
+link = buck.get_download_url(PDF_NAME)
 
 # insert link into README.
 
